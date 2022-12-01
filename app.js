177f3b009c9b62cd9548a4e4ae266f9e4f6de69e
Nasir Ansari <115993017+Nasir-Technical@users.noreply.github.com>
2022-12-01 15:20:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+// var a = ["a","b","c","d","e"]
+// a.push("f","g","h")
+
+
+// a.pop()
+// a.shift()
+// a.unshift("x")
+// var b = a.slice(0,6)
+// var c = a.splice(3,4)
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// a.splice(2,1,"n","k")
+
+// // var d = ["a","b","c","d","e","f" ,"a","b","c","d","e","f", "a","b","c","d","e","f" ,"a","b","c","d","e","f"]
+// // d.splice(3,0,"p","i")
+// // console.log(d);
+
+// var u = d.indexOf("d")
+// console.log(d[u])
+
+// var x = d.length;
+// console.log(x)
+ 
+
+// for (var a=0; a < 10; a++){
+//     console.log("3 X " + (a+1) + " = " + 3*(a+1))
+// }
+// function abc(a){
+//     console.log(a);
+// }
+// abc("abc");
+
+
+// var count = 0;
+
+
+// function addCount (){
+//     count = 1;
+// }
+// addCount();
+// console.log(count);
+
+
+
+
+
+
+// function abc(){
+//     var a = 10;
+//     return a;
+// }
+//   var c = abc();
+//   console.log(c);
+
+
+// var d = prompt("DAY NAME:")
+// alert("day")
+// switch (day){
+//     case "sun":
+//         console.log('holiday')
+//         break;
+//         case "mon":
+//          console.log("first day of week");
+//         break;
+//         case "tue":
+//             console.log("second day of week ")
+//             break;
+//             case "sat":
+//                 console.log("open day of offices");
+//                 break;
+//                 default:
+//                     console.log("no day found");
+// }
+
+// var i = 0;
+// while(i < 9){
+//     i++;
+//     console.log(i);
+    
+// }
+
+
+
+// var i = 0;
+// do{
+//     i++;
+//     console.log(i);
+    
+// }while(i < 10);ay
+// function abc(a){
+//     console.log(a);
+// }
+
+
+
+
+
+
+// var  b = []
+// var textInput = document.getElementById("inp")
+// function addValue(){
+//     console.log(b);
+//     // console.log(textInput.value);
+//     a.push("New Value")
+// }
+
+
+
+
+
+
+
+function abc(a){
+   console.log(a)
+}
+
+function calculator(){
+    var a = eval(display.value);
+    console.log(a)
+    display.value += a;
+    
+}
+
+
+
+// function abc(a){
+//     console.log(a)
+//     img.src = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+// }
+
+
+// function cba(b){
+//     console.log(b)
+//     img.src ="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg";
+// }
